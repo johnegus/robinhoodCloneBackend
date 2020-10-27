11dcf18f02df7609446ea04b30aa7673179a7163
john <jgh2102@gmail.com>
2020-10-28 03:17:29 +0900

    fetchable positions

---
+++ db/position-repository.js
@@ -0,0 +1,41 @@
+const { Position } = require("./models");
+
+
+async function create(details, user) {
+    details.userId = user.id;
+    const position = await Position.create(details);
+    return position.id;
+  }
+  
+  async function list() {
+    return await Position.findAll({
+      attributes: ["stockSymbol", "stockName", "currentPrice", "id"],
+    });
+  }
+  
+  async function one(id) {
+    const position = await Position.findByPk(id, {
+      include: ["user"],
+    });
+  
+    return {
+        stockSymbol: position.stockSymbol,
+        stockName: position.stockName,
+        currentPrice: currentPrice.id,
+        id: position.id,
+        buyPrice: position.buyPrice,
+        shares: position.shares,
+      user: {
+        id: position.user.id,
+        name: position.user.name,
+      },
+      updatedAt: position.updatedAt,
+    };
+  }
+  
+  module.exports = {
+    create,
+    list,
+    one,
+  };
+  
--- routes/api/positions.js
+++ routes/api/positions.js
@@ -9,16 +9,25 @@ const router = express.Router();
 const db = require('../../db/models');
 
 const { Position } = db;
+const PositionRepository = require('../../db/position-repository');
 
+router.get(
+  "/",
+  authenticated,
+  asyncHandler(async function (_req, res) {
+    const positions = await PositionRepository.list();
+    res.json(positions);
+  })
+);
 
-router.get('/', asyncHandler(async (req, res) => {
-  const positions = await Position.findAll({
-    order: [['createdAt', 'ASC']],
-    attributes: ['id', 'stockSymbol', 'stockName', 'currentPrice',
-    'buyPrice', 'shares'],
-  });
-  res.json({ positions });
-}));
+// router.get('/', asyncHandler(async (req, res) => {
+//   const positions = await Position.findAll({
+//     order: [['createdAt', 'ASC']],
+//     attributes: ['id', 'stockSymbol', 'stockName', 'currentPrice',
+//     'buyPrice', 'shares'],
+//   });
+//   res.json({ positions });
+// }));
 
 
 
