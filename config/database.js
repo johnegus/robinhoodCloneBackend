721446085e35c781b61301410514786b603d935b
john <jgh2102@gmail.com>
2020-11-02 12:44:53 +0900

    ready to push to heroku

--- config/database.js
+++ config/database.js
@@ -13,17 +13,10 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
-    seederStorage: 'sequelize',
-  },
-  test: {
-    dialect: "sqlite",
-    DB_CONN: "sqlite.memory",
-    logging: false,
-    seederStorage: 'sequelize',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
-  },
+  }
 };
