1e1bc49a41aa2f7cbadbeb8db156ea2caa3fcdea
john <jgh2102@gmail.com>
2020-11-22 07:16:37 +0900

    changed history seed data for charting

--- db/seeders/20201102095145-history.js
+++ db/seeders/20201102095145-history.js
@@ -6,10 +6,10 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Histories', [
-      { deposit: 10000, userId: 1, createdAt: new Date(), updatedAt: new Date(), },
-      { stockSymbol: 'TSLA', stockName: 'Tesla Motors', boughtPrice: 250,  soldPrice: 2500, shares: 4,  userId: 1, createdAt: new Date(), updatedAt: new Date(), },
-      { stockSymbol: 'NFLX', stockName: 'Netflix', boughtPrice: 250, soldPrice: 500,  shares: 5, userId: 1, createdAt: new Date(), updatedAt: new Date(), },
-      { stockSymbol: 'AMZN', stockName: 'Amazon', boughtPrice: 250, soldPrice: 150,  shares: 8, userId: 1, createdAt: new Date(), updatedAt: new Date(), },
+      { deposit: 10000, userId: 1, boughtPrice: 0, soldPrice: 0, shares: 0, createdAt: new Date(), updatedAt: new Date(), },
+      { deposit: 0, stockSymbol: 'TSLA', stockName: 'Tesla Motors', boughtPrice: 250,  soldPrice: 2500, shares: 4,  userId: 1, createdAt: new Date(), updatedAt: new Date(), },
+      { deposit: 0, stockSymbol: 'NFLX', stockName: 'Netflix', boughtPrice: 250, soldPrice: 500,  shares: 5, userId: 1, createdAt: new Date(), updatedAt: new Date(), },
+      { deposit: 0, stockSymbol: 'AMZN', stockName: 'Amazon', boughtPrice: 250, soldPrice: 150,  shares: 8, userId: 1, createdAt: new Date(), updatedAt: new Date(), },
     ]);
   },
 
